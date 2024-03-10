2ad769d4b878b1644debc0a14b36982b04f28d44
andrey <raptor1053@yandex.ru>
2024-03-10 13:17:50 +0200

    prepare for first data and first page

--- testtask/coworkers/models.py
+++ testtask/coworkers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from mptt.models import TreeForeignKey
+
+
+class Coworker(models.Model):
+    pib = models.CharField(max_length=254)
+    position = models.CharField(max_length=254)
+    start_date = models.DateTimeField(default=timezone.now)
+    email = models.EmailField(max_length=254)
+    headman = TreeForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children')
+
+    def serialize(self):
+        data = {'id': self.id, 'pib': self.pib, 'position': self.position,
+                'start_date':self.start_date, 'email': self.email}
+        if self.headman_id:
+            data['headman'] = {'id': self.headman_id, 'pib': self.headman.pib}
+        return data
--- testtask/coworkers/views.py
+++ testtask/coworkers/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from coworkers.models import Coworker
+
+
+def main_page(request):
+    return render(request, template_name='main_page.html')
