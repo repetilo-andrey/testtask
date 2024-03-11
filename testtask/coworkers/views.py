d5e53c205e366cbcdb59dc434af4f6bbe952ab19
andrey <raptor1053@yandex.ru>
2024-03-11 13:48:38 +0200

    hierarchy page

--- testtask/coworkers/views.py
+++ testtask/coworkers/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.urls import reverse
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 from coworkers.models import Coworker
 
@@ -8,5 +14,19 @@ def main_page(request):
     show_list = []
     for root_coworker in root_coworkers:
         show_list.append([root_coworker.serialize_short(), root_coworker.get_descendants().filter(level=1).order_by('pib')])
-    return render(request, template_name='hierarchy.html', context={'coworkers': show_list})
+    context = {'coworkers': show_list, 'more_coworkers_url': reverse('more_coworkers')}
+    return render(request, template_name='hierarchy.html', context=context)
 
+
+@api_view(['GET'])
+def more_coworkers(request):
+    obj_id = request.query_params.get('obj_id')
+    coworker = Coworker.objects.filter(id=obj_id).first()
+    if not coworker:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    lower_coworkers = coworker.get_descendants().filter(level__gt=coworker.level)
+    for c in lower_coworkers:
+        print (c.id, c.pib, c.level, c.parent_id)
+    data = {'coworkers': lower_coworkers, 'parent_id': obj_id}
+    coworkers_html = render_to_string('hierarchy_more_coworkers.html', data)
+    return Response({'html': coworkers_html}, status=status.HTTP_200_OK)
--- testtask/testtask/urls.py
+++ testtask/testtask/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from coworkers.views import main_page
+from coworkers.views import main_page, more_coworkers
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_page),
+    path('more_coworkers/', more_coworkers, name='more_coworkers'),
 ]
 
 if settings.DEBUG:
